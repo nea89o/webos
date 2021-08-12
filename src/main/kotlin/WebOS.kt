2ee4b7c2bef9f80ed12e58bc3cdbb965299920e1
nea <romangraef@gmail.com>
2021-08-12 23:37:47 +0200

    file system rework

--- src/main/kotlin/WebOS.kt
+++ src/main/kotlin/WebOS.kt
@@ -29,15 +29,15 @@ class Console(val os: WebOS, val renderElement: Element?) {
 
 	var shouldRerender = true
 
-	lateinit var currentUser: User
+	var currentUser: User? = null
 
-	private var _workingDirectory: AbsolutPath? = null
+	private var _workingDirectory: Path.Absolute? = null
 
-	var workingDirectory: AbsolutPath
-		get() {
-			if (_workingDirectory == null) _workingDirectory = currentUser.homeDirectory; return _workingDirectory!!
+	var workingDirectory: Path.Absolute
+		get() = _workingDirectory ?: currentUser?.homeDirectory ?: Path.root
+		set(value) {
+			_workingDirectory = value
 		}
-		set(value) { _workingDirectory = value }
 
 	fun openActivity(activity: Activity) {
 		activityStack.addLast(activity)
@@ -67,7 +67,7 @@ class Console(val os: WebOS, val renderElement: Element?) {
 class WebOS {
 	private val _consoles = mutableListOf<Console>()
 	val consoles get() = _consoles.toList()
-	val IOHandler = IOHandler()
+	val files = IOHandler()
 	fun registerConsole(element: Element) {
 		_consoles.add(Console(this, element))
 	}
@@ -75,92 +75,6 @@ class WebOS {
 
 data class User(
 	val name: String,
-	val homeDirectory: AbsolutPath
+	val homeDirectory: Path.Absolute
 )
 
-data class RelativePath internal constructor(override val parts: List<String>) : Path {
-	override fun toAbsolutPath(context: Console): AbsolutPath {
-		val result = mutableListOf<String>()
-		for (part in parts) {
-			when (part) {
-				"." -> result.addAll(context.workingDirectory.parts)
-				"~" -> result.addAll(context.currentUser.homeDirectory.parts)
-				".." -> { result.removeLast() }
-				else -> result.add(part)
-			}
-		}
-		return AbsolutPath(result)
-	}
-}
-
-data class AbsolutPath internal constructor(override val parts: List<String>) : Path {
-	override fun toAbsolutPath(context: Console): AbsolutPath = this
-}
-
-sealed interface Path {
-	val parts: List<String>
-	fun toAbsolutPath(context: Console): AbsolutPath
-	companion object {
-		fun of(string: String): Path {
-			val isAbsolut = string.first() == '/'
-			val parts = string.split("/")
-			val aparts = parts.subList(1, parts.size)
-			if (aparts.contains(".") || aparts.contains("~"))
-				throw IllegalArgumentException("'.' and '~' are only allowed at the beginning of a relative path")
-			if (!isAbsolut) return RelativePath(parts)
-			if (aparts.contains("..")) throw IllegalArgumentException("'..' is only allowed in a relative path")
-			return AbsolutPath(aparts)
-		}
-	}
-}
-
-class FileSystem {
-	fun read(relativePath: Path): ReadResult = TODO()
-	fun write(path: Path, data: ByteArray): Unit = TODO()
-	fun stat(path: Path): Unit = TODO()
-}
-
-class IOHandler {
-	val mounts = mutableListOf<Mount>()
-	fun mount(absolutPath: AbsolutPath, fileSystem: FileSystem) {
-		mounts += Mount(absolutPath, fileSystem)
-	}
-	fun unmount(mountPoint: AbsolutPath) {
-		mounts.removeAll { it.mountPoint == mountPoint }
-	}
-	fun <T> findMountFor(context: Console, path: Path, operation: FileSystem.(relativePath: Path) -> T): T {
-		val absolutPath = path.toAbsolutPath(context)
-		val mount = mounts.filter {
-			var result = true
-			it.mountPoint.parts.forEachIndexed { index, part -> if (absolutPath.parts[index] != part) {
-				result = false
-			} }
-			result
-		}.maxByOrNull { it.mountPoint.parts.size} ?: throw IllegalStateException("No mount present")
-		return mount.fileSystem.operation(
-			AbsolutPath(absolutPath.parts.subList(mount.mountPoint.parts.size, absolutPath.parts.size))
-		)
-	}
-
-	fun read(context: Console, path: Path): ReadResult = findMountFor(context, path) { read(it) }
-	fun write(context: Console, path: Path, data: ByteArray): Unit = findMountFor(context, path) { write(it, data) }
-	fun stat(context: Console, path: Path): Unit = findMountFor(context, path) { stat(it) }
-}
-
-sealed class ReadResult {
-	class Success(val text: String) : ReadResult()
-	object NotFound
-	object NoAccess
-}
-data class Mount (
-	val mountPoint: AbsolutPath,
-	val fileSystem: FileSystem
-)
-
-data class Stat(
-	val exists: Boolean,
-	var owner: User,
-	val created: Long,
-	val edited: Long,
-	val size: Long
-)
