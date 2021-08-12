06d9212e5901b8b73e17bb8938b0c4542a42770b
nea <romangraef@gmail.com>
2021-08-13 00:54:39 +0200

    test basics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,32 @@
 plugins {
-    kotlin("js") version "1.5.21"
+	kotlin("multiplatform") version "1.5.21"
+	//id("io.kotest.multiplatform") version "5.0.0.3"
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
+	maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
-kotlin {
-    js {
-        browser {
+val kotestVersion: String by project
 
-        }
-    }
+kotlin {
+	targets {
+		js(IR) {
+			nodejs { }
+			browser { }
+		}
+	}
+	sourceSets {
+		val jsMain by getting {
+
+		}
+		val jsTest by getting {
+			dependencies {
+				implementation("io.kotest:kotest-assertions-core:4.6.1")
+				implementation("io.kotest:kotest-framework-api:4.6.1")
+				implementation("io.kotest:kotest-framework-engine:4.6.1")
+			}
+		}
+	}
 }
--- src/main/kotlin/util/sequence.kt
+++
@@ -1,5 +0,0 @@
-package util
-
-fun <T> Iterable<T>.expandWith(t: T): Sequence<T> =
-	this.asSequence() + generateSequence { t }.asSequence()
-
